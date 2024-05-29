8ecb28fe33066e8112622eb3a3cbdc58997c436d
Isac <isac3299@gmail.com>
2024-05-28 23:46:59 -0300

    queria ter feito mais

--- obt_pr/urls.py
+++ obt_pr/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_experimentos, name='lista_experimentos'),
-    path('experimento/<int:experimento_id>/', views.detalhe_experimento, name='detalhe_experimento'),
+    path('experimento/<int:pk>/', views.detalhe_experimento, name='detalhe_experimento'),
 ]
